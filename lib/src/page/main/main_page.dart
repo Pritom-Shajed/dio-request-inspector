c3e975bca3532a832336b19512525d9c1fc14cde
Pritom Shajed <pritomshajed@gmail.com>
2025-07-24 11:54:53 +0600

    modified: show on long press bool added

--- lib/src/page/main/main_page.dart
+++ lib/src/page/main/main_page.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 
 class DioRequestInspectorMain extends StatelessWidget {
   final Widget child;
+  final bool showOnLongPress;
   final DioRequestInspector inspector;
 
-  const DioRequestInspectorMain(
-      {Key? key, required this.child, required this.inspector})
-      : super(key: key);
+  const DioRequestInspectorMain({Key? key, required this.child, required this.inspector, this.showOnLongPress = true})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onLongPress: () {
+        if (!showOnLongPress) return;
         if (!inspector.isInspectorEnabled) {
           return;
         }
